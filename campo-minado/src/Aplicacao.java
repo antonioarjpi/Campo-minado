63aba485a3f5c2962a2c1bbc0ff704020ecc9553
antonioarjpi <antonioarjpi@gmail.com>
2022-01-23 19:38:00 -0300

    Subindo projeto

---
+++ campo-minado/src/Aplicacao.java
@@ -0,0 +1,10 @@
+import modelo.Tabuleiro;
+import visao.TabuleiroConsole;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 3);
+        new TabuleiroConsole(tabuleiro);
+    }
+}
